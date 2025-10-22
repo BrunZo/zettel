dac65a402f026afd83ce5cff0f3f35738c0ad31f
BrunZo <ziger.bruno@gmail.com>
2025-10-21 21:13:56 -0300

    add: semif-full display and default show* set to false

--- ui/zettel_grid.tsx
+++ ui/zettel_grid.tsx
@@ -5,7 +5,7 @@ export default function ZettelGrid({ zettels }: { zettels: ZettelMeta[] }) {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
       {zettels.map((zettel) => (
-        <Zettel key={zettel.id} {...zettel} mode="card" />
+        <Zettel key={zettel.id} {...zettel} mode="card" showDate={true} showTags={true} showAbstract={true} />
       ))}
     </div>
   );
