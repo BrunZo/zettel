9267cf4b6b443f01dd510e885c68c6affbdecb9a
BrunZo <ziger.bruno@gmail.com>
2025-12-18 00:57:54 -0300

    feat: better zettel grid styles

--- ui/z/zettel_grid.tsx
+++ ui/z/zettel_grid.tsx
@@ -2,11 +2,11 @@ import { ZettelMeta } from "../../lib/types";
 import ZettelDisplay from "./zettel";
 
 export default function ZettelGrid({ zettels }: { zettels: ZettelMeta[] }) {
-    return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-            {zettels.map((zettel) => (
-                <ZettelDisplay key={zettel.id} {...zettel} mode="card" />
-            ))}
-        </div>
-    );
+  return (
+    <div className='flex flex-col'>
+      {zettels.map((zettel) => (
+        <ZettelDisplay key={zettel.id} {...zettel} mode="card" />
+      ))}
+    </div>
+  );
 }
